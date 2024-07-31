0f6180b1428a345a0357a4e204ff5de810353733
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 19:00:24 +0530

    Create FindOdd.java

---
+++ FindOdd.java
@@ -0,0 +1,9 @@
+public class FindOdd{
+// Prepare a Logic to print odd didgit in anumber
+  // Example if we have taken Number 122456 it should return digits which are in odd position in this number it should retrun odd placed values 
+  // 1 
+  // 2
+  // 5
+  // This will be the output since they are in odd positions
+  
+}
