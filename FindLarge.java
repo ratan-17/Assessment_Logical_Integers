a24a10ea88f44b2922f6af6fb23ab5ed3fd1dff5
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 19:04:02 +0530

    Create FindLarge.java

---
+++ FindLarge.java
@@ -0,0 +1,5 @@
+public class FindLarge{
+// prepare a Logic to findout Large digit in given Number
+  // If Number is 1782 The Largest Digit is 8 You should prepare logic to print that Number
+  // Take Number By using Scanner
+  }
