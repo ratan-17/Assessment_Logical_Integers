c2e7106f2af4243b6e53eb0c402d2e050fad07df
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 19:11:47 +0530

    Create FindEven.java

---
+++ FindEven.java
@@ -0,0 +1,5 @@
+public class FindEven{
+// Prepare a Logic to find out that a Digit in a Number should be Even Digit and Print Even Digit
+  // 1298
+  // if we take above number it should print 2 and 8 because both are even digits in a number
+}
