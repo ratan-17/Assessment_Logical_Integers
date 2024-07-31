7fb648f0d5aee49a4c97b7658d87a49adf6f160c
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 19:06:57 +0530

    Create FindSum.java

---
+++ FindSum.java
@@ -0,0 +1,6 @@
+public class FindSum{
+// Prepare a Logic to Findout the Sum of Prime Placed Digits
+  // Example if we take 34578 as a Number we should consider 3+5+8=16 it should print 16 as Answer
+  // LikeWise Prepare a Logic to find out sum at prime placed Integer
+  // Take Number Using Scanner
+}
