122e476259370d08ee2d78e492581d5a48f7f38a
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-31 18:52:39 +0530

    Create FindNum.java

---
+++ FindNum.java
@@ -0,0 +1,6 @@
+public FindNum{
+// Prepare a Logic to find out 2nd  number in a set of number 
+  // Let's Take a Number in Scanner
+  // Example if you take 235 as number it should return 3
+  // Likewise Logic you have written should get 2nd digit of any number
+}
